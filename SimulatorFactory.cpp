bb5df6addc50c73e24b145231d2fec07eb74ec99
Abhinav Kshitij <akshitij@asu.edu>
2020-04-30 16:10:16 -0700

    Created Simulator; runs but has bugs

---
+++ SimulatorFactory.cpp
@@ -0,0 +1,301 @@
+#include <iostream>
+#include <stdexcept>
+#include <cmath>
+#include <cstring>
+#include <iterator>
+#include <algorithm>
+#include <vector>
+#include "SimulatorFactory.h"
+
+// Client function: transferOnGPIB
+void transferOnGPIB(GPIBInstrument& from, GPIBInstrument& to)
+{
+	to.send(from.receive());
+}
+
+// Interface destructor: Controller
+GPIBController::~GPIBController(){};
+
+// // DERIVED Class: Controller 
+// void GPIBController_GC::insert(const char* device_name, unsigned int address)
+// {
+// 	std::cout << device_name << " now at address " << address << std::endl;
+// }
+// void GPIBController_GC::send(unsigned int address, const char* cmd)
+// {
+// 	std::cout << "GPIB instrument # " << address << " sends " << cmd
+// 	<< std::endl;
+// }
+// void GPIBController_GC::send(unsigned int address, float f)
+// {
+// 	std::cout << "GPIB instrument# " << address << " sends value " << f 
+// 	<< std::endl;
+// }
+// float GPIBController_GC::receive(unsigned int address)
+// {
+// 	std::cout << "Please enter number for GPIB instrument #" << address << ": ";
+// 	float f;
+// 	std::cin >> f;
+// 	return f;
+// }
+	
+
+// Destructor: GPIBInstrumentSimulation
+GPIBInstrumentSimulation::~GPIBInstrumentSimulation(){}
+
+
+void ExperimentSimulation::apply(double voltage){}
+double ExperimentSimulation::test(){return the_voltage;}	
+
+
+Acme130Simulation::Acme130Simulation(ExperimentSimulation& e)
+	: the_experiment(e)
+{
+	// my_controller.insert("Acme130Simulation", what_address);
+}
+
+void Acme130Simulation::send(const char*)
+{
+	//
+}
+
+void Acme130Simulation::send(float f)
+{
+	the_experiment.apply(f);
+}
+
+float Acme130Simulation::receive()
+{
+	//
+}
+
+
+VoltyMetricsSimulation::VoltyMetricsSimulation(ExperimentSimulation& e)
+	: the_experiment(e)
+{
+	// my_controller.insert("VoltyMetrics", what_address);
+}
+
+void VoltyMetricsSimulation::send(const char*)
+{
+	//
+}
+
+void VoltyMetricsSimulation::send(float f)
+{
+	//
+}
+
+float VoltyMetricsSimulation::receive()
+{
+	return the_experiment.test();
+}
+
+SimulatorFactory::~SimulatorFactory(){}
+
+
+// GPIBSimulation constructor
+GPIBController_GIS::GPIBController_GIS(const SimulatorFactory& factory)
+	: simulator_factory(factory), simulators(31)
+{
+	simulators = {};
+}
+
+GPIBController_GIS::~GPIBController_GIS()
+{
+	for (int i = 0; i < simulators.size(); ++i)
+	{
+		delete simulators[i];
+	}
+}
+
+// Insert function: Call factory to register a new intrument to the array.
+void GPIBController_GIS::insert(const char* device_name, unsigned int address)
+{
+	simulators[address] = simulator_factory.create(device_name, the_experiment);
+}
+
+void GPIBController_GIS::send(unsigned int address, const char* msg)
+{
+	GPIBInstrumentSimulation* ip = simulators[address];
+	if(ip!=0)
+	{
+		ip -> send(msg);
+	}
+	else
+	{
+		throw std::invalid_argument("No simulator at specified GPIB address");
+	}
+}
+
+void GPIBController_GIS::send(unsigned int address, float f)
+{
+	GPIBInstrumentSimulation *ip = simulators[address];
+	if(ip!=0)
+	{
+		ip -> send(f);
+	}
+	else
+	{
+		throw std::invalid_argument("No simulator at specified GPIB address");
+	}
+}
+
+float GPIBController_GIS::receive(unsigned int address)
+{
+	float val;
+	GPIBInstrumentSimulation* ip = simulators[address];
+	if(ip!=0)
+	{
+		val = ip -> receive();
+	}
+	else
+	{
+		throw std::invalid_argument("No simulator at specified GPIB address");
+	}
+}
+
+GPIBInstrumentSimulation* 
+	MySimulators::create(const char* device_name, ExperimentSimulation& exp) const
+{
+	GPIBInstrumentSimulation* sim_p;
+	if(strcmp(device_name, "Acme130_VS_GI_GC") == 0)
+		sim_p = new Acme130Simulation(exp);
+	else if(strcmp(device_name, "VoltyMetrics_VM_GI") == 0)
+		sim_p = new VoltyMetricsSimulation(exp);
+	else
+		throw std::invalid_argument("Simulator for specified device unavailable");
+		sim_p = NULL;
+	// return EXIT_SUCCESS;
+}	
+
+
+// Interface destructor: Voltage Supply
+VoltageSupply::~VoltageSupply(){};
+
+
+// Voltage supply 1: ACME 130
+Acme130_VS_GI_GC::Acme130_VS_GI_GC(GPIBController& controller, int gpib_address)
+		: my_controller(controller), my_gpib_address(gpib_address)
+{
+	my_controller.insert("Acme130_VS_GI_GC", gpib_address);
+}
+
+void Acme130_VS_GI_GC::set(float volts)
+{
+	if (volts > maximum() || volts < minimum()) 
+		throw std::invalid_argument("Acme130_VS_GI_GC voltage out of range");
+	my_controller.send(my_gpib_address, volts); // send(): declared in GPIBInterface; defined in Acme130 class 
+}
+
+float Acme130_VS_GI_GC::minimum() const {return 0.0;}
+float Acme130_VS_GI_GC::maximum() const {return 40.0;}
+
+// GPIBInstrument interface
+void Acme130_VS_GI_GC::send(const char* cmd)
+{
+	my_controller.send(my_gpib_address, cmd);
+}
+void Acme130_VS_GI_GC::send(float volts) 
+{
+	my_controller.send(my_gpib_address, volts);
+}
+float Acme130_VS_GI_GC::receive() 
+{
+	 return my_controller.receive(my_gpib_address);
+}	 
+
+// Voltage supply 2: VoltOn59_VS_GI_GC
+ VoltOn59_VS_GI_GC::VoltOn59_VS_GI_GC(GPIBController& controller, int gpib_address)
+		: my_controller(controller), my_gpib_address(gpib_address)
+{
+	my_controller.insert("VoltOn59_VS_GI_GC", gpib_address);
+}
+
+void VoltOn59_VS_GI_GC::set(float volts)
+{
+	if (volts > maximum() || volts < minimum()) 
+		throw std::invalid_argument("VoltOn59_VS_GI_GC voltage out of range");
+	my_controller.send(my_gpib_address, volts);
+}
+
+float VoltOn59_VS_GI_GC::minimum() const {return 0.0;}
+float VoltOn59_VS_GI_GC::maximum() const {return 40.0;}
+
+// GPIBInstrument interface
+void VoltOn59_VS_GI_GC::send(const char* cmd)
+{
+	my_controller.send(my_gpib_address, cmd);
+}
+void VoltOn59_VS_GI_GC::send(float volts) 
+{
+	my_controller.send(my_gpib_address, volts);
+}
+float VoltOn59_VS_GI_GC::receive() 
+{
+	 return my_controller.receive(my_gpib_address);
+}
+
+// INTERFACE destructor: Voltmeter
+Voltmeter::~Voltmeter(){};
+
+
+VoltyMetrics_VM_GI::VoltyMetrics_VM_GI(GPIBController& controller, int what_address)
+	: my_controller(controller), my_gpib_address(what_address)
+{
+	my_controller.insert("VoltyMetrics_VM_GI", what_address);
+}
+
+float VoltyMetrics_VM_GI::read()
+{
+	return my_controller.receive(my_gpib_address);
+}
+
+
+// CLIENT FUNCTION
+float checkCalibration(VoltageSupply& supply, Voltmeter& meter, float test_voltage)
+{
+	supply.set(test_voltage);
+	return abs(test_voltage - meter.read()) / test_voltage;
+}
+
+
+// CLIENT CLASS
+IVTester::IVTester(VoltageSupply& vs, Voltmeter& vm)
+	: the_voltage_supply(vs), the_voltmeter(vm)
+{
+	// empty constructor
+}
+
+double IVTester::current(double voltage)
+{
+	the_voltage_supply.set(voltage);
+	return the_voltmeter.read();
+}
+
+
+
+
+
+// DRIVER CODE
+int main(int argc, char const *argv[])
+{
+	MySimulators simulators;
+	GPIBController_GIS gpib(simulators);
+	
+	// Check calibration 
+	Acme130_VS_GI_GC supply(gpib, 12);
+	VoltyMetrics_VM_GI meter(gpib, 13);
+
+	IVTester iv(supply, meter);
+	double v_step = 1.0;
+	for (int i = 0; i < 10; ++i)
+	{
+		std::cout << iv.current(v_step * i) << std::endl;
+	}
+
+
+
+	return 0;
+}
+
---
+++ SimulatorFactory.h
@@ -0,0 +1,218 @@
+#include <iostream>
+#include <iterator>
+#include <vector>
+
+// VIRTUAL INTERFACE: Instrument controller
+class GPIBInstrument
+{
+public:
+ 	virtual void send(const char* cmd) = 0;
+ 	virtual void send(float f) = 0;
+	virtual float receive() = 0;
+};
+
+// Client function:
+void transferOnGPIB(GPIBInstrument& from, GPIBInstrument& to);
+
+// BASE INTERFACE CLASS
+class GPIBController
+{
+public:
+	virtual void insert(const char* device_name, unsigned int address) = 0;
+ 	virtual void send(unsigned int address, const char* cmd) = 0;
+ 	virtual void send(unsigned int address, float f) = 0;
+	virtual float receive(unsigned int address) = 0; 
+	virtual ~GPIBController();	
+};
+
+// // DERIVED CLASS
+// class GPIBController_GC: public GPIBController
+// {
+// public:
+// 	virtual void insert(const char* device_name, unsigned int address) override;
+//  	virtual void send(unsigned int address, const char* cmd) override;
+//  	virtual void send(unsigned int address, float f) override;
+// 	virtual float receive(unsigned int address) override; 
+// };
+
+
+// BASE INTERFACE CLASS
+class GPIBInstrumentSimulation
+{
+public:
+	virtual void send(const char*) = 0;
+	virtual void send(float f) = 0;
+	virtual float receive() = 0;
+
+	virtual ~GPIBInstrumentSimulation();
+};
+
+// SHARED OBJECT
+class ExperimentSimulation
+{
+private:
+	double the_voltage;
+public:
+	void apply(double voltage);
+	double test();	
+};
+
+// DERIVED CLASS
+class Acme130Simulation: public GPIBInstrumentSimulation
+{
+private:
+	ExperimentSimulation& the_experiment;
+public:
+	Acme130Simulation(ExperimentSimulation& e);
+	virtual void send(const char*);
+	virtual void send(float f);
+	virtual float receive();	
+};
+
+// DERIVED CLASS
+class VoltyMetricsSimulation: public GPIBInstrumentSimulation
+{
+private:
+	ExperimentSimulation& the_experiment;
+public:
+	VoltyMetricsSimulation(ExperimentSimulation& e);
+	virtual void send(const char*);
+	virtual void send(float f);
+	virtual float receive();	
+};
+
+
+// INTERFACE BASE CLASS: SIMULATOR FACTORY 
+class SimulatorFactory
+{
+public:
+	virtual GPIBInstrumentSimulation* 
+		create(const char* device_name, ExperimentSimulation& exp) const = 0;
+	virtual ~SimulatorFactory();
+};
+
+
+// DERIVED INTFERFACE FROM BASE CONTROLLER
+class GPIBController_GIS: public GPIBController
+{
+private:
+	const SimulatorFactory& simulator_factory;
+	std::vector<GPIBInstrumentSimulation*> simulators;
+	ExperimentSimulation the_experiment;
+public:
+	GPIBController_GIS(const SimulatorFactory& factory);
+	~GPIBController_GIS();
+
+	// GPIBController INTERFACE
+	virtual void insert(const char* device_name, unsigned int address) override;
+	virtual void send(unsigned int address, const char* buf) override;
+	virtual void send(unsigned int address, float f) override;
+	virtual float receive (unsigned int address) override;
+};
+
+
+// DERIVED CLASS: SIMULATOR FACTORY
+class MySimulators: public SimulatorFactory
+{
+public:
+	virtual GPIBInstrumentSimulation* 
+		create(const char* device_name, ExperimentSimulation& exp) const override;
+};
+
+
+
+// BASE INTERFACE CLASS
+class VoltageSupply
+{
+public:
+	virtual void set(float volts) = 0;
+	virtual float minimum() const = 0;
+	virtual float maximum() const = 0;
+	virtual ~VoltageSupply();
+};
+
+// DERIVED CLASS 1: Voltage supply 1
+class Acme130_VS_GI_GC: public VoltageSupply, public GPIBInstrument
+{
+private:
+	GPIBController& my_controller; // member interface: Controller
+	int my_gpib_address;
+public:
+	Acme130_VS_GI_GC(GPIBController& controller, int gpib_address);
+
+	// VoltageSupply Interface
+	virtual void set(float volts) override;
+	virtual float minimum() const override;
+	virtual float maximum() const override;
+
+	// GPIBInstrument interface
+	virtual void send(const char* cmd) override;
+ 	virtual void send(float f) override;
+	virtual float receive() override;
+};
+
+
+// DERIVED CLASS 2: Voltage supply 2
+class VoltOn59_VS_GI_GC: public VoltageSupply, public GPIBInstrument
+{
+private:
+	GPIBController& my_controller; // member interface: Controller
+	int my_gpib_address;
+public:
+	VoltOn59_VS_GI_GC(GPIBController& controller, int gpib_address);
+
+	// VoltageSupply Interface
+	virtual void set(float volts) override;
+	virtual float minimum() const override;
+	virtual float maximum() const override;
+
+	// GPIBInstrument interface
+	virtual void send(const char* cmd) override;
+ 	virtual void send(float f) override;
+	virtual float receive() override;
+};
+
+
+// BASE INTERFACE CLASS
+class Voltmeter
+{
+public:
+	virtual float read() = 0;
+	~Voltmeter();
+};
+
+// DERIVED CLASS: Multiple inheritence
+class VoltyMetrics_VM_GI: public Voltmeter
+{
+private:
+	GPIBController& my_controller;
+	int my_gpib_address;	
+public:
+	VoltyMetrics_VM_GI(GPIBController& controller, int what_address);
+
+	// Voltmeter interface
+	virtual float read() override;
+};
+
+
+// Client function calling interface base classes VoltageSupply, Voltmeter 
+float checkCalibration(VoltageSupply& supply, 
+					   Voltmeter& meter, 
+					   float test_voltage);
+
+
+// CONCRETE CLASS
+class IVTester
+{
+private:
+	VoltageSupply& the_voltage_supply;
+	Voltmeter& the_voltmeter;
+public:
+	IVTester(VoltageSupply& vs, Voltmeter& vm);
+	double current(double voltage);
+};
+
+
+
+
+
